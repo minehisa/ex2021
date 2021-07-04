b96e39938333d69d46375907377ea997481de078
KohtaSugimoto <i846009a@mails.cc.ehime-u.ac.jp>
2021-07-04 09:32:05 +0900

    DBへの追加・ログイン

---
+++ UI_program/UI_峯久朋也/db_new_register.php
@@ -0,0 +1,43 @@
+<?php
+
+if(isset($_POST['address']) && isset($_POST['pass'])){
+
+    $address = $_POST['address'];
+    $pass = $_POST['pass'];
+
+    // DB接続情報
+    $dsn = "pgsql:host=133.71.3.90; dbname=practice;";
+    $username = "dbuser";
+
+    // try-catch
+    try{
+        // データベースへの接続を表すPDOインスタンスを生成
+        $dbh = new PDO($dsn, $username);
+
+        $salt = 'abcde12345abcde12345zz';
+        $cost = 12;
+        //SHA-512ハッシュ
+        $hash = crypt( $pass, '$6$' . $cost . '$' . $salt . '$');
+        
+        // usertbにデータ追加
+        $sql = "INSERT INTO usertb(address, password) VALUES ('$address', '$hash')";
+
+        $data = $dbh->query($sql); 
+
+        if( !empty($data) ) {
+            echo $address, "を登録しました。";
+        }else{
+            echo $address, "は登録できませんでした。";
+        }
+    } catch(PDOException $e) {
+        
+        echo $e->getMessage();
+        die();
+    }
+    // 接続を閉じる
+    $dbh = null;
+}else{
+    echo "アドレスとパスワードを両方入力してください。";
+}
+
+?>
